1263fd08e771e54e18a9c236ce39edfadd07a3f8
oguz <oguz.trk49@gmail.com>
2024-12-31 00:51:08 +0300

    Add project files.

---
+++ 01_TemelKonular/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_TemelKonular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("oguz");
+           
+
+            string isim = "oguz";
+            Console.WriteLine(isim); 
+            Console.Read();
+        }
+    }
+}
